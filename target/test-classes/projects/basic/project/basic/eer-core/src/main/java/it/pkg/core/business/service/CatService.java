d8ada37471093c85f17303e1e60fc5aca5da426c
Eduardo Estrella <kapow4@gmail.com>
2017-03-30 17:32:15 +0200

    Remove target classes

--- target/test-classes/projects/basic/project/basic/eer-core/src/main/java/it/pkg/core/business/service/CatService.java
+++
@@ -1,9 +0,0 @@
-package it.pkg.core.business.service;
-
-import it.pkg.core.business.service.dto.CatDto;
-
-import java.util.List;
-
-public interface CatService {
-    List<CatDto> getHelloCat();
-}
--- target/test-classes/projects/basic/project/basic/eer-web/src/main/java/it/pkg/web/rest/application/HelloApplication.java
+++
@@ -1,8 +0,0 @@
-package it.pkg.web.rest.application;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class HelloApplication extends Application {
-}
